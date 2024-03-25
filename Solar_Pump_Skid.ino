4b2c3b89abf1c0ece91f524bdb4799ed2952d7c6
Gunnar <157182026+gunnski@users.noreply.github.com>
2024-03-25 16:29:06 -0700

    Update on code development
    2024-03-25 Progress Update: Sorting out the architecture and functionality

--- Solar_Pump_Skid.ino
+++ Solar_Pump_Skid.ino
@@ -8,29 +8,57 @@
   By: Gunnar Blomquist
 */
 
-#include <Arduino_EdgeControl.h>    // general library for edge
+#include <Arduino_EdgeControl.h>  // general library for edge
 // #include "Helpers.h"             // library for RTC
+#include <list>  // for zone Ordering control
 
-int state = 0;
+
+int state = 0;  // Finite State Machine
 bool pressureError = false;
 bool lowVoltageError = false;
-int pressure = 0;
-int batteryPercent = 100;
+double pressure = 0;
+double voltqge = 0;
+double batteryPercentage = 0;
+
+std::list<int> zoneOrder = { 1, 2, 3, 4, 5, 6 };
+int valveZone = 1;
+int numberOfZones = 6;
+
+const int minBatteryPercent = 20;    // the minumim discharge level of battery in percentage
+const int maxBatteryPercent = 80;    // the necessary battery percentage threshold needed to operate the system
+const int minimumPumpPressure = 10;  // minumin allowed pump pressure in psi
+const int PumpDuration = 15;         // Duration of pump operation in minutes
+
+void nextZone() {
+  /* Advance the queue when called */
+  valveZone = (valveZone > numberOfZones) ? valveZone = 1 : ++valveZone;
+}
+
+enum State {
+  ERRORSTATE,
+  IDLE,
+  PUMPSTART,
+  PUMPRUN,
+  PUMPSTOP
+};
+
+void error() {
+  // call this state when water tank is empty and wait for reset
+
+};
 
 void idle() {
-  if (!pressureError) {
-
-    // meassure voltage here
-
-    // battery life in %
-    if (batteryPercent >= 20) {
-
-      // Check time is between 4:00-6:00 AM
-
+  // meassure voltage here
+  // batteryPercentage = map(voltage, 0,255, 0, 100);
+  // battery life in %
+  if (batteryPercentage >= maxBatteryPercent) {
+    if (!pressureError) {
+      // Check time??
+    } else {
+      state = IDLE;
     }
-    else {
-      state = 1; // Idle
-    }
+  } else {
+    state = IDLE;
   }
 }
 
@@ -40,46 +68,59 @@ void pumpStart() {
 
   // get pressure from 4-20ma input here
 
-  if ((pressureStartTime - millis()) > 5000 and pressure < 10) {
+  if ((pressureStartTime - millis()) > 5000 && pressure < 10) {
     pressureError = true;
-    state = 4; // pumpStop
-
-    else {
-      state = 3; // pumpRun
-    }
+    state = PUMPSTOP;
+  } else {
+    state = PUMPSTART;
   }
+}
 
-  void pumpRun() {
-    // check pressure is >= 10 psi via pressure input
-    // check battery life is >= 20% via voltage input
+void pumpRun() {
+  // check pressure is >= 10 psi via pressure input
+  // check battery life is >= 20% via voltage input
+  unsigned long pumpRunMillis = millis();
+  if ((pumpRunMillis - millis()) > PumpDuration * 60 * 1000) {
+    nextZone();
+    state = PUMPSTOP;
   }
-
-  void pumpStop() {
-    // turn valve & inverter off
-
+  if (pressure < minimumPumpPressure || batteryPercentage < minBatteryPercent) {
+    state = PUMPSTOP;
+  } else {
+    state = PUMPRUN;
   }
+}
 
-  void setup() {
+void pumpStop() {
+  // turn valve & inverter off
+}
 
+int pumpSolenoid = HIGH;
+int valveOne;
+int valveTwo;
+
+void setup() {
+  // pumpSolenoid = pinMode(1,OUTPUT)
+  // valveOne = pinMode(2,OUTPUT);
+  // valveTwo = pinMode(3,OUTPUT);
+}
+
+void loop() {
+  switch (state) {
+    case ERRORSTATE:
+      error();
+      break;
+    case IDLE:
+      idle();
+      break;
+    case PUMPSTART:
+      pumpStart();
+      break;
+    case PUMPRUN:
+      pumpRun();
+      break;
+    case PUMPSTOP:
+      pumpStop();
+      break;
   }
-
-  void loop() {
-    switch (state) {
-      case (1): {
-          idle();
-          break;
-        }
-      case (2): {
-          pumpStart();
-          break;
-        }
-      case (3): {
-          pumpRun();
-          break;
-        }
-      case (4): {
-          pumpStop();
-          break;
-        }
-    }
-  }
+}
